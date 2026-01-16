43ac559aaff8730028edaba66a9e48b924b0d786
Menaka J <menakaraman4@gmail.com>
2026-01-16 20:59:36 +0530

    c3

--- src/main/java/com/jvlcode/spring_boot_demo/controllers/UserController.java
+++ src/main/java/com/jvlcode/spring_boot_demo/controllers/UserController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     //testing /api/users works
+    public String getUsers() {
+        return "Hello from Menaka"
+    }
 
 
 }
