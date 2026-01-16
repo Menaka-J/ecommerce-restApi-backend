523cfc47fbac7b62d5d7fd2070f5f62b83b118d6
Menaka J <menakaraman4@gmail.com>
2026-01-16 21:17:37 +0530

    add list

--- src/main/java/com/jvlcode/spring_boot_demo/controllers/UserController.java
+++ src/main/java/com/jvlcode/spring_boot_demo/controllers/UserController.java
@@ -1,18 +1,26 @@
 package com.jvlcode.spring_boot_demo.controllers;
 
+import com.jvlcode.spring_boot_demo.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
-    @GetMapping
-    //testing /api/users works in url
-    public String getUsers() {
-        return "Hello from Menaka";
-    }
+//    @GetMapping
+//    //testing /api/users works in url
+//    public String getUsers() {
+//        return "Hello from Menaka";
+//    }
 
+    @GetMapping
+    public List<User> getUsers() {
+        return Arrays.asList(new User(1L, "menaka", "men@gmail.com"), new User(2L, "meena", "meena@gmail.com"));
+    }
 
 }
