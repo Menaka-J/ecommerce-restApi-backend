01c360642b2231c9ce4fc96ac7c4302da4e495c1
Menaka J <menakaraman4@gmail.com>
2026-01-16 20:55:45 +0530

    commit one

---
+++ src/main/java/com/jvlcode/spring_boot_demo/controllers/UserController.java
@@ -0,0 +1,11 @@
+package com.jvlcode.spring_boot_demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+
+}
