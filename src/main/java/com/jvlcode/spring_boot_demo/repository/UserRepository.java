08ad6397a989fe6da3530832ec3794888efb2145
Menaka J <menakaraman4@gmail.com>
2026-01-18 22:27:42 +0530

    table rep7

--- src/main/java/com/jvlcode/spring_boot_demo/repository/UserRepository.java
+++ src/main/java/com/jvlcode/spring_boot_demo/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.jvlcode.spring_boot_demo.repository;
 import com.jvlcode.spring_boot_demo.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }
 
 //repository is used between controller and entity
