ba3066ddeaf0510a00150b3f3fb871044630b1f8
Menaka J <menakaraman4@gmail.com>
2026-01-18 22:31:33 +0530

    table g&s

--- src/main/java/com/jvlcode/spring_boot_demo/entity/UserEntity.java
+++ src/main/java/com/jvlcode/spring_boot_demo/entity/UserEntity.java
@@ -20,6 +20,29 @@ public class UserEntity {
         this.email = email;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     //default constructor
     public UserEntity() {
