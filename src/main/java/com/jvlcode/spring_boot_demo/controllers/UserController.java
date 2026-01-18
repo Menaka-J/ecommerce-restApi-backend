e44f99b6625006c3701f8793b786715f3b9247ff
Menaka J <menakaraman4@gmail.com>
2026-01-18 22:25:49 +0530

    table rep5

--- src/main/java/com/jvlcode/spring_boot_demo/controllers/UserController.java
+++ src/main/java/com/jvlcode/spring_boot_demo/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.jvlcode.spring_boot_demo.controllers;
 
 import com.jvlcode.spring_boot_demo.model.User;
+import com.jvlcode.spring_boot_demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+
+    @Autowired
+    private UserRepository userRepository;
 
 //    @GetMapping
 //    //testing /api/users works in url
