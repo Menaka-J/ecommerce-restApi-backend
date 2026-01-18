0d48f88105dccf2fb9fbbf659cc72920dcc2c022
Menaka J <menakaraman4@gmail.com>
2026-01-18 22:05:02 +0530

    add entity

--- src/main/java/com/jvlcode/spring_boot_demo/entity/UserEntity.java
+++ src/main/java/com/jvlcode/spring_boot_demo/entity/UserEntity.java
@@ -1,4 +1,9 @@
 package com.jvlcode.spring_boot_demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class UserEntity {
 }
