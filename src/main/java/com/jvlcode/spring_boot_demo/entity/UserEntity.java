ab12646e158a3ddf548706dbf4699b21212716e8
Menaka J <menakaraman4@gmail.com>
2026-01-18 22:05:49 +0530

    table annotation

--- src/main/java/com/jvlcode/spring_boot_demo/entity/UserEntity.java
+++ src/main/java/com/jvlcode/spring_boot_demo/entity/UserEntity.java
@@ -1,9 +1,12 @@
 package com.jvlcode.spring_boot_demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
 }
