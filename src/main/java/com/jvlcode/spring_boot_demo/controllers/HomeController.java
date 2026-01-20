36805bdcecb29b62fccc8881c2805cbefafb54a1
Menaka J <menakaraman4@gmail.com>
2026-01-21 00:26:04 +0530

    21-05

--- src/main/java/com/jvlcode/spring_boot_demo/controllers/HomeController.java
+++ src/main/java/com/jvlcode/spring_boot_demo/controllers/HomeController.java
@@ -1,4 +1,15 @@
 package com.jvlcode.spring_boot_demo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
 public class HomeController {
+
+    @GetMapping
+    public String getHomePage() {
+        return "Welcome to Home!";
+    }
 }
