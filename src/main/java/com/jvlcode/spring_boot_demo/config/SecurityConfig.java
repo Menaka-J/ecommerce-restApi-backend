dd10909dcf27bf70f1dfd6cb80118cfec5e0c428
Menaka J <menakaraman4@gmail.com>
2026-01-21 00:39:59 +0530

    21-09

--- src/main/java/com/jvlcode/spring_boot_demo/config/SecurityConfig.java
+++ src/main/java/com/jvlcode/spring_boot_demo/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.jvlcode.spring_boot_demo.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,9 +11,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity //used for enabling security
 public class SecurityConfig {
 
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        http.authorizeHttpRequests(authz -> authz.requestMatchers("/api/users/**").authenticated());
-        return http.build();
+    @Bean //using Bean , this class can be used in others code also
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) { //get http which has all url
+        http.authorizeHttpRequests(authz ->
+                authz.requestMatchers("/api/users/**").authenticated() //tells we only want auth for this url's
+                        .requestMatchers("/home").permitAll() //tells we permit all to visit for this url's
+        );
+        return http.build(); //tells SB we did this so build it
     }
 
 }
