f764921f198da798547007200f05fdf8c5a6402a
Menaka J <menakaraman4@gmail.com>
2026-01-21 00:48:35 +0530

    21-11

--- src/main/java/com/jvlcode/spring_boot_demo/config/SecurityConfig.java
+++ src/main/java/com/jvlcode/spring_boot_demo/config/SecurityConfig.java
@@ -12,11 +12,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean //using Bean , this class can be used in others code also
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{ //get http which has all url
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { //get http which has all url
         http.authorizeHttpRequests(authz ->
                 authz.requestMatchers("/api/users/**").authenticated() //tells we only want auth for this url's
-                        .requestMatchers("/").permitAll() //tells we permit all to visit for this url's
-        );
+                        .requestMatchers("/").permitAll() //tells we permit all to visit for this url's[here Home page]
+        ).formLogin(form -> form.permitAll()); //permit default login page
         return http.build(); //tells SB we did this so build it
     }
 
