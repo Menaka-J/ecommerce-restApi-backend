e266f0d640699db6e1ead99f2b7fcd3db7b98bf7
Menaka J <menakaraman4@gmail.com>
2026-01-21 00:35:48 +0530

    21-078

--- src/main/java/com/jvlcode/spring_boot_demo/config/SecurityConfig.java
+++ src/main/java/com/jvlcode/spring_boot_demo/config/SecurityConfig.java
@@ -1,4 +1,18 @@
 package com.jvlcode.spring_boot_demo.config;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration //tell we are ging to add imp config's
+@EnableWebSecurity //used for enabling security
 public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http.authorizeHttpRequests(authz -> authz.requestMatchers("/api/users/**").authenticated());
+        return http.build();
+    }
+
 }
