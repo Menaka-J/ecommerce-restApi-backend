97978741f78fe04ff76db6b28b91b45bd0c61a76
Menaka J <menakaraman4@gmail.com>
2026-01-21 22:48:46 +0530

    21a-07

--- src/main/java/com/jvlcode/spring_boot_demo/repository/UserRepository.java
+++ src/main/java/com/jvlcode/spring_boot_demo/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.jvlcode.spring_boot_demo.repository;
 import com.jvlcode.spring_boot_demo.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
 }
 
 //repository is used between controller and entity
