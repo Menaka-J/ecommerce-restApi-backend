eca3dd843c3fd82d718fb110596c560f9d74b72a
Menaka J <menakaraman4@gmail.com>
2026-01-21 22:32:20 +0530

    21a-04

--- src/main/java/com/jvlcode/spring_boot_demo/entity/UserEntity.java
+++ src/main/java/com/jvlcode/spring_boot_demo/entity/UserEntity.java
@@ -11,13 +11,18 @@ public class UserEntity {
     private Long id;
     private String name;
     private String email;
+    private String username;
+    private String password;
+
 
     //    constructor for initialization
-    public UserEntity(Long id, String name, String email) {
+    public UserEntity(Long id, String name, String email, String username, String password) {
         super();
         this.id = id;
         this.name = name;
         this.email = email;
+        this.username = username;
+        this.password = password;
     }
 
     //getters and setters for all 3 var
@@ -43,6 +48,22 @@ public class UserEntity {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     //default constructor
