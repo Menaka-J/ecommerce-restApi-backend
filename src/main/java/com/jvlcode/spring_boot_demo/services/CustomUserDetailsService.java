e00b5498116eb5d1df5bac7fcbe48195714b21d7
Menaka J <menakaraman4@gmail.com>
2026-01-21 22:54:44 +0530

    21a-08

--- src/main/java/com/jvlcode/spring_boot_demo/services/CustomUserDetailsService.java
+++ src/main/java/com/jvlcode/spring_boot_demo/services/CustomUserDetailsService.java
@@ -1,14 +1,25 @@
 package com.jvlcode.spring_boot_demo.services;
 
+import com.jvlcode.spring_boot_demo.entity.UserEntity;
+import com.jvlcode.spring_boot_demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class CustomUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        //fetch user from database
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new User(user.getUsername(), user.getPassword(),null);
     }
 }
 
