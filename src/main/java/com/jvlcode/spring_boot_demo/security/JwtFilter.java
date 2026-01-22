46808b873d20546cd206a543fb9d86087ef51711
Menaka J <menakaraman4@gmail.com>
2026-01-22 15:17:00 +0530

    22-24

--- src/main/java/com/jvlcode/spring_boot_demo/security/JwtFilter.java
+++ src/main/java/com/jvlcode/spring_boot_demo/security/JwtFilter.java
@@ -1,4 +1,35 @@
 package com.jvlcode.spring_boot_demo.security;
 
-public class JwtFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token authHeader.substring(7);
+
+        try {
+
+        } catch (Exception e) {
+
+        }
+    }
 }
