f1959e61fb8f7fa72ebd1d94a15f5b7127945774
Menaka J <menakaraman4@gmail.com>
2026-01-22 14:18:12 +0530

    22-04

--- src/main/java/com/jvlcode/spring_boot_demo/security/JwtUtil.java
+++ src/main/java/com/jvlcode/spring_boot_demo/security/JwtUtil.java
@@ -1,4 +1,14 @@
 package com.jvlcode.spring_boot_demo.security;
 
+import io.jsonwebtoken.security.Keys;
+
 public class JwtUtil {
+
+    private static final String SECRET_KEY_STRING = "Z8xseQyBJv1Yi65UaT5bkDKeiua98R2o";  //secret jey for our site
+
+    private final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(SECRET_KEY_STRING.getBytes()); //covert key to 256 bits for use
+
+
+
+
 }
