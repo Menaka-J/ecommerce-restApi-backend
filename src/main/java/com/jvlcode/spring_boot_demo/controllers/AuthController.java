317315c0dc65d3d0c1c9ac1c40522ab71cf15869
Menaka J <menakaraman4@gmail.com>
2026-01-22 14:39:40 +0530

    22-12

--- src/main/java/com/jvlcode/spring_boot_demo/controllers/AuthController.java
+++ src/main/java/com/jvlcode/spring_boot_demo/controllers/AuthController.java
@@ -4,10 +4,13 @@ import com.jvlcode.spring_boot_demo.entity.UserEntity;
 import com.jvlcode.spring_boot_demo.security.JwtUtil;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth") //localhost:8080/auth
@@ -19,13 +22,15 @@ public class AuthController {
     private JwtUtil jwtUtil;
 
     @PostMapping("/login")     // auth/login
-    public void login(@RequestBody UserEntity user) {
+    public ResponseEntity<Map<String, String>> login(@RequestBody UserEntity user) {
 
         //authenticate the user
         Authentication authentication = authManager
                 .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())); //tells JWT to authenticate or not
 
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String token = jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(Map.of("token", token));
     }
 
 }
