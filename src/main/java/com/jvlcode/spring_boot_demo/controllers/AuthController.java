554c456a89eeb1ac9ada18e0d1b9f7fd2950941f
Menaka J <menakaraman4@gmail.com>
2026-01-22 14:37:57 +0530

    22-11

--- src/main/java/com/jvlcode/spring_boot_demo/controllers/AuthController.java
+++ src/main/java/com/jvlcode/spring_boot_demo/controllers/AuthController.java
@@ -1,10 +1,12 @@
 package com.jvlcode.spring_boot_demo.controllers;
 
 import com.jvlcode.spring_boot_demo.entity.UserEntity;
+import com.jvlcode.spring_boot_demo.security.JwtUtil;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,6 +15,8 @@ public class AuthController {
 
     @Autowired
     private AuthenticationManager authManager;
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @PostMapping("/login")     // auth/login
     public void login(@RequestBody UserEntity user) {
@@ -21,7 +25,7 @@ public class AuthController {
         Authentication authentication = authManager
                 .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())); //tells JWT to authenticate or not
 
-
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
     }
 
 }
