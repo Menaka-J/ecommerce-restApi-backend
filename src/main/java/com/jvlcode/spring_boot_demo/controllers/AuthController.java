44293e844b455a8c94f01128f4171ded76627251
Menaka J <menakaraman4@gmail.com>
2026-01-22 14:28:13 +0530

    22-08

--- src/main/java/com/jvlcode/spring_boot_demo/controllers/AuthController.java
+++ src/main/java/com/jvlcode/spring_boot_demo/controllers/AuthController.java
@@ -1,4 +1,16 @@
 package com.jvlcode.spring_boot_demo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth") //localhost:8080/auth
 public class AuthController {
+
+    @GetMapping("/login") /// auth/login
+    public void login(){
+
+    }
+
 }
