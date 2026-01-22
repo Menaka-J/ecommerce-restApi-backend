7e9f7a5d5b751c3e5f92af3ff324fd7f0722c986
Menaka J <menakaraman4@gmail.com>
2026-01-22 14:48:27 +0530

    22-16

--- src/main/java/com/jvlcode/spring_boot_demo/config/SecurityConfig.java
+++ src/main/java/com/jvlcode/spring_boot_demo/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -70,7 +71,7 @@ public class SecurityConfig {
 
     //form chatgpt for AUthCOntroller autowired probelm
     @Bean
-    public AuthenticationManager authenticationManager() throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
 
